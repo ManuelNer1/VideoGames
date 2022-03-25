022dce1748b17393d43f2ce49e316e8f4a6fcaec
Jose Cobos <josepablocobos.a@gmail.com>
2022-03-25 17:39:40 -0600

    Add the last changes to the code in order to validate de position

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -12,6 +12,7 @@ from turtle import *
 
 from freegames import line
 
+import numpy as np 
 
 def grid():
     """Draw tic-tac-toe grid."""
@@ -23,6 +24,9 @@ def grid():
 
 def drawx(x, y):
     """Draw X player."""
+    # print("Valor de x: " + str(x))
+    # print("Valor de y: " + str(y))
+
     pencolor('blue')
     line(x, y , x+40 , y+40)
     line(x, y+40 , x+40 , y)
@@ -31,10 +35,11 @@ def drawx(x, y):
 def drawo(x, y):
     """Draw O player."""
     up()
+
     goto(x+15,y)
     down()
-    print("Valor de x: " + str(x))
-    print("Valor de y: " + str(y))
+    # print("Valor de x: " + str(x))
+    # print("Valor de y: " + str(y))
     pencolor('red')
     circle(15)
 
@@ -50,20 +55,40 @@ players = [drawx, drawo]
 
 def tap(x, y):
     """Draw X or O in tapped square."""
-    x = floor(x)
-    y = floor(y)
+    global c
+    global quadx
+    global quady
+    i= 0
+    v = 0
+
+
+
+    x = int(floor(x))
+    y = int(floor(y))
+    # print("Valor de x: " + str(x))
+    # print("Valor de y: " + str(y))
+
+    if x in quadx and y in quady:
+        print("Dato Invalido")
+        return False
+    elif c<9:
+        quadx[0][c]=x
+        quady[0][c]=y
+      
+
+
     player = state['player']
     draw = players[player]
     draw(x, y)
     update()
     state['player'] = not player
-
+c= 0
+quadx=np.zeros (shape = (1,9))
+quady=np.zeros (shape = (1,9))
 setup(420, 420, 370, 0)
-
 hideturtle()
 tracer(False)
 grid()
 update()
 onscreenclick(tap)
-
 done()
